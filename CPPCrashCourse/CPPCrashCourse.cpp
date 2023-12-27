efe5375435ed807051b65859a002051ab726ecde
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2023-12-27 13:38:04 -0300

    Adicionar arquivos de projeto.

---
+++ CPPCrashCourse/CPPCrashCourse.cpp
@@ -0,0 +1,12 @@
+﻿// CPPCrashCourse.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "CPPCrashCourse.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
